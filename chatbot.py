9b3a6df3fd729a8124d43b6131970e167f671f59
git-ksupriya <ksupriya2430@gmail.com>
2026-04-19 21:36:27 +0530

    fix: improved memory by replacing summary with json updation

--- chatbot.py
+++ chatbot.py
@@ -2,33 +2,56 @@
 from dotenv import load_dotenv 
 import os
 from groq import Groq
+import json
 
 load_dotenv()
 groq_key=os.getenv("groq_api_key")
 c=True
 
+patient_data={"name":"", "age":None, "medical_history":[], "previous_admissions_in_last_yr":None}
+
 history=[{"role":"system","content":"speak 1 word"}]
-long_history=[{"role":"system","content":"speak exactly 1 word only"}]
 
 client=Groq(api_key=groq_key)
-count=0
-while c==True:
-    count+=1
 
+def chat():
     user=input("User: ")
     history.append({"role":"user","content":user})
+
     response=client.chat.completions.create( messages=history, model="llama-3.3-70b-versatile")     #send user response to bot and get its reply   you are a healthcare assistant who will take data and make a prediction if person will be readmitted"
     bot=response.choices[0].message.content
     print('Bot: ',bot)
 
-    long_history.append({"role":"user","content":user})
     history.append({"role":"assistant","content":bot})
-    long_history.append({"role":"assistant","content":bot})
+    
+def convert_to_json(raw):
+    modified_update=raw.replace("```json", "")
+    modified_update=modified_update.replace("```", "")
+    modified_update=modified_update.strip()
+    modified_update=modified_update.replace("'",'"')
+    return modified_update
+
+
+def update(patient_data,history):
+    updation=client.chat.completions.create(model="llama-3.3-70b-versatile",messages=[{"role":"user","content":f"update data in JSON ONLY (not python): {patient_data}, given recent history: {history[2:]}"}])
+    raw_update=(updation.choices[0].message.content)
+    modified_update=convert_to_json(raw_update)
+
+    patient_data=json.loads(modified_update)
+    print(modified_update, patient_data)
+
+    history[1]={"role":"system","content":f"patient data: {patient_data}"}
+
+
+count=0
+while c==True:
+    count+=1
+    chat()
+
 
     if count>5:
-        summarize=client.chat.completions.create(model="llama-3.3-70b-versatile",messages=[{"role":"user","content":f"summarise this: {long_history}"}])
-        summary=summarize.choices[0].message.content
-        history[1]={"role":"system","content":f"summary: {summary}"}
+        update(patient_data,history)
+        count=0
 
     if len(history)>6:
         history=history[0:2]+history[-5:]
