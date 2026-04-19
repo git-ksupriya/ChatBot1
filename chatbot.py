e6da0e36394573f16139ba244d218bb3ed953ec8
git-ksupriya <ksupriya2430@gmail.com>
2026-04-19 13:38:57 +0530

    fix: improved memory using periodic summaries

--- chatbot.py
+++ chatbot.py
@@ -2,22 +2,36 @@
 from dotenv import load_dotenv 
 import os
 from groq import Groq
+
 load_dotenv()
 groq_key=os.getenv("groq_api_key")
 c=True
 
 history=[{"role":"system","content":"speak 1 word"}]
+long_history=[{"role":"system","content":"speak exactly 1 word only"}]
+
 client=Groq(api_key=groq_key)
+count=0
 while c==True:
+    count+=1
+
     user=input("User: ")
     history.append({"role":"user","content":user})
     response=client.chat.completions.create( messages=history, model="llama-3.3-70b-versatile")     #send user response to bot and get its reply   you are a healthcare assistant who will take data and make a prediction if person will be readmitted"
     bot=response.choices[0].message.content
-    history.append({"role":"assistant","content":bot})
-    if len(history)>6:
-        _=history.pop(1)
-        _=history.pop(1)
     print('Bot: ',bot)
+
+    long_history.append({"role":"user","content":user})
+    history.append({"role":"assistant","content":bot})
+    long_history.append({"role":"assistant","content":bot})
+
+    if count>5:
+        summarize=client.chat.completions.create(model="llama-3.3-70b-versatile",messages=[{"role":"user","content":f"summarise this: {long_history}"}])
+        summary=summarize.choices[0].message.content
+        history[1]={"role":"system","content":summary}
+
+    if len(history)>6:
+        history=history[0:2]+history[-5:]
     c=input("Continue? y/n")
     c=False if c=='n' else True
 
